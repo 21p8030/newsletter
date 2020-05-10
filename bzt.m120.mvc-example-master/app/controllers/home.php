62a33dbd1e39b57cbac3357161941fdd23469aa7
julienwyss <44972519+julienwyss@users.noreply.github.com>
2020-05-10 21:25:28 +0200

    homework

--- bzt.m120.mvc-example-master/app/controllers/home.php
+++ bzt.m120.mvc-example-master/app/controllers/home.php
@@ -26,14 +26,50 @@ class Home extends Controller {
         //schauen ob mail schon registriert
         $write = true;
         while(!feof($file)){
-                if(trim(fgets($file)) == $_POST['Email']){
+                if(trim(fgets($file)) == "Mail ".$_POST['Email']){
+                    //Mail adresse wurde bereits erfasst also muss nichts geschrieben werden
                     $write = false;
-                }
+                    //exit;
+                }    
         }
+
+        while(!feof($file)){
+            if($write == true){
+                if(trim(fgets($file)) == "Vorname: ".$_POST['Vorname']){
+                    if(trim(fgets($file)) == "Nachname: ".$_POST['Nachname']){
+                        if(trim(fgets($file)) == "Strasse: ".$_POST['Strasse']){
+                            if(trim(fgets($file)) == "Ort: ".$_POST['Ort']){
+                                echo "Alles gleich ausser Mail";
+                                confirm("Email Adresse überschreiben");
+                            } 
+                        } 
+                    }
+                }
+            }   
+        }
+     //   $newslettertest = array($_POST['Email'],$_POST['Vorname'],$_POST['Nachname'],$_POST['Strasse'],$_POST['Ort']);
+        
+        
+       // $anzahl = count ( $newslettertest );
+       // echo "<p>Es gibt $anzahl Einträge</p>";
+       // echo "<ul>";
+         
+       /* for ($x = 0; $x < $anzahl; $x++)
+        {
+            echo "<li>Eintrag von $x ist $newslettertest[$x] </li>";
+        }
+         
+        echo "</ul>";
+        */
 
         // fehler meldung oder mail in file schreiben
         if($write == true){
-            fwrite($file, $_POST['Email']."\n");
+           // fwrite($file, $newslettertest);
+           fwrite($file, "Mail ".$_POST['Email']."\n");
+            fwrite($file, "Vorname: ".$_POST['Vorname']."\n");
+            fwrite($file, "Nachname: ".$_POST['Nachname']."\n");
+            fwrite($file, "Strasse: ".$_POST['Strasse']."\n");
+            fwrite($file, "Ort: ".$_POST['Ort']."\n");
             echo $_POST['Email'].' wurde für Sie erfasst';
         }else{
             echo $_POST['Email'].' wurde bereits erfasst';
--- bzt.m120.mvc-example-master/app/views/home/newsletter.php
+++ bzt.m120.mvc-example-master/app/views/home/newsletter.php
@@ -1,4 +1,7 @@
-<form action="newsletter//bzt.m120.mvc-example-master/public/home/newsletter" method="POST">
+<form action="/newsletter/bzt.m120.mvc-example-master/public/home/newsletter" method="POST">
+	Vorname: <input name="Vorname" type="text" /> Nachname: <input name="Nachname" type="text" /><br />
+	
+	Strasse: <input name="Strasse" type="text" /> Ort: <input name="Ort" type="text" /><br />
     E-mail:<br>
 	<input name="Email" type="text" /><br /> <br/>
 
